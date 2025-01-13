e2efc37b1e64f64996f0a7495d7f0e5ccbda3330
Leonardo Irwin Plaça <leoplaca2010@gmail.com>
2025-01-13 17:13:17 -0300

    changing the return to print the hole request

--- server.js
+++ server.js
@@ -11,72 +11,90 @@ app.post('/requisicao', (req, res) => {
     // Exibe o corpo da requisição no console
     console.log('Corpo da requisição recebido:');
     console.log(req.body);  // Aqui é onde o corpo da requisição é mostrado
+    console.log('REQ inteira: ');
+    console.log(req);
 
     // Responde com uma mensagem de sucesso
-        res.status(200).send('Corpo da requisição recebido com sucesso' + res.status);
+    res.status(200).send(req);
 });
 
 app.put('/requisicao', (req, res) => {
     // Exibe o corpo da requisição no console
     console.log('Corpo da requisição recebido:');
+    
     console.log(req.body);  // Aqui é onde o corpo da requisição é mostrado
+    console.log('REQ inteira: ');
+    console.log(req);
 
     // Responde com uma mensagem de sucesso
-        res.status(200).send('Corpo da requisição recebido com sucesso' + res.status);
+    res.status(200).send(req);
 });
 
 app.get('/requisicao', (req, res) => {
     // Exibe o corpo da requisição no console
     console.log('Corpo da requisição recebido:');
     console.log(req.body);  // Aqui é onde o corpo da requisição é mostrado
+    console.log('REQ inteira: ');
+    console.log(req);
 
     // Responde com uma mensagem de sucesso
-        res.status(200).send('Corpo da requisição recebido com sucesso' + res.status);
+    res.status(200).send(req);
 });
 
 app.options('/requisicao', (req, res) => {
     // Exibe o corpo da requisição no console
     console.log('Corpo da requisição recebido:');
     console.log(req.body);  // Aqui é onde o corpo da requisição é mostrado
+    console.log('REQ inteira: ');
+    console.log(req);
 
     // Responde com uma mensagem de sucesso
-        res.status(200).send('Corpo da requisição recebido com sucesso' + res.status);
+    res.status(200).send(req);
 });
 
 app.trace('/requisicao', (req, res) => {
     // Exibe o corpo da requisição no console
     console.log('Corpo da requisição recebido:');
     console.log(req.body);  // Aqui é onde o corpo da requisição é mostrado
+    console.log('REQ inteira: ');
+    console.log(req);
 
     // Responde com uma mensagem de sucesso
-    res.status(200).send('Corpo da requisição recebido com sucesso' + req.protocol);
+    res.status(200).send(req);
 });
 
 app.patch('/requisicao', (req, res) => {
     // Exibe o corpo da requisição no console
     console.log('Corpo da requisição recebido:');
     console.log(req.body);  // Aqui é onde o corpo da requisição é mostrado
+    console.log('REQ inteira: ');
+    console.log(req);
 
     // Responde com uma mensagem de sucesso
-    res.status(200).send('Corpo da requisição recebido com sucesso' + req.protocol);
+    res.status(200).send(req);
 });
 
 app.head('/requisicao', (req, res) => {
     // Exibe o corpo da requisição no console
     console.log('Corpo da requisição recebido:');
     console.log(req.body);  // Aqui é onde o corpo da requisição é mostrado
+    console.log('REQ inteira: ');
+    console.log(req);
 
     // Responde com uma mensagem de sucesso
-    res.status(200).send('Corpo da requisição recebido com sucesso' + req.protocol);
+    res.status(200).send(req);
 });
 
 app.delete('/requisicao', (req, res) => {
     // Exibe o corpo da requisição no console
     console.log('Corpo da requisição recebido:');
     console.log(req.body);  // Aqui é onde o corpo da requisição é mostrado
+    console.log('REQ inteira: ');
+    console.log(req);
 
     // Responde com uma mensagem de sucesso
-    res.status(200).send('Corpo da requisição recebido com sucesso' + req.protocol);
+    res.status(200).send(req);
+
 });
 
 
